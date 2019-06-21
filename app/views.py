e01e2e1b67e8473b1294a8e1a460b8d1b3ac50a5
Vivvianne Kimani <ndiahav@gmail.com>
2019-06-21 14:54:21 +0300

    add variable block and dynamic route on view

--- app/views.py
+++ app/views.py
@@ -2,10 +2,12 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/')
-def index():
+@app.route('/news/<news_id>')
+def index(news_id):
 
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = 'News app'
+    
+    return render_template('index.html',message = message)
