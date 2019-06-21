e50202d0840820d982aee78b01783ab35ead5959
Vivvianne Kimani <ndiahav@gmail.com>
2019-06-21 15:21:01 +0300

    add content on index.html

--- app/views.py
+++ app/views.py
@@ -8,14 +8,16 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    
+    title = 'Home - Welcome to The best Movie Review Website Online'
     message = 'News app'
     
-    return render_template('index.html',message = message)
+    return render_template('index.html',title = title)
 
 @app.route('/news/<int:news_id>')
 def movie(news_id):
 
     '''
-    View movie page function that returns the movie details page and its data
+    View movie page function that returns the news details page and its data
     '''
-    return render_template('movie.html',id = news_id)
+    return render_template('news.html',id = news_id)
