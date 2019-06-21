365fd5336d267171ca95cbb94371bb4de7e90558
Vivvianne Kimani <ndiahav@gmail.com>
2019-06-21 15:04:51 +0300

    set up configuration classes

--- app/views.py
+++ app/views.py
@@ -2,8 +2,8 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/news/<news_id>')
-def index(news_id):
+@app.route('/')
+def index():
 
     '''
     View root page function that returns the index page and its data
@@ -11,3 +11,11 @@ def index(news_id):
     message = 'News app'
     
     return render_template('index.html',message = message)
+
+@app.route('/news/<int:news_id>')
+def movie(news_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html',id = news_id)
